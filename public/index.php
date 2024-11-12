805f8f30918200951e83aff2d535568acfbe81f7
ahnafi <devsulthon@gmail.com>
2024-11-12 17:19:43 +0700

    feat : flasher alert

--- public/index.php
+++ public/index.php
@@ -5,7 +5,14 @@ use Config\Database;
 use App\Router;
 use Controller\Home;
 
+//set database to prod
 Database::getConnection("prod");
+
+//set timezone
+date_default_timezone_set("Asia/jakarta");
+
+//set session for flasher message
+if(!session_id()) session_start();
 
 Router::add("GET", "/", Home::class, 'home' );
 Router::add("GET", "/about", Home::class, 'about' );
