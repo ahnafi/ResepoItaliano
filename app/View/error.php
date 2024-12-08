5779431f2005ad465fb94454451a76b3b5537890
ahnafi <devsulthon@gmail.com>
2024-12-08 14:39:06 +0700

    add : 404 page

--- app/View/error.php
+++ app/View/error.php
@@ -1,6 +1,27 @@
-<div>
-    <h1>Error 404: Page Not Found</h1>
-    Dalam <span id="time">10</span> detik redirect ke beranda
+<?php include_once __DIR__ . '/Components/navbar.php'; ?>
+
+<style>
+    .error {
+        min-height: calc(100vh - 3rem);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .error img {
+        max-width: 376px;
+        margin-top: 4rem;
+    }
+
+</style>
+
+<div class="error">
+    <img src="/images/elements/error.svg" alt="error 404">
+    <h1 class="title-font-size">Halaman Tidak Ditemukan</h1>
+    <p class="small-font-size">
+        Dalam <span id="time">10</span> detik akan diarahkan ke beranda
+    </p>
 </div>
 
 <script>
